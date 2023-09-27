6c069457b3f3f1ffd5c10587d1cbb0fdccb711b5
larynx <larynx.austrene@gmail.com>
2023-09-27 23:09:26 +0200

    Switched to EsiPy for ESI Authentication and made this thing a discord bot!

--- assets.py
+++ assets.py
@@ -1,10 +1,6 @@
-import asyncio
 from collections import defaultdict
 
 import yaml
-from tqdm.asyncio import tqdm_asyncio
-
-from esi.endpoints import get_item_name, get_names, get_assets
 
 
 def good_location(location_flag):
@@ -18,12 +14,22 @@ def good_location(location_flag):
 
 
 class Assets:
-    def __init__(self, character_id, access_token, character=True):
+    def __init__(self, character_id, esi_app, esi_client, character_name=""):
+        self.character_id = character_id
+        self.character_name = character_name
 
-        if character:
-            self.assets = asyncio.run(get_assets(character_id, access_token))
-        else:
-            raise NotImplementedError
+        # Fetch assets
+        self.assets = []
+        op = esi_app.op['get_characters_character_id_assets'](character_id=character_id)
+        head_result = esi_client.head(op)
+
+        if head_result.status == 200:
+            npages = head_result.header['X-Pages'][0]
+            ops = [esi_app.op['get_characters_character_id_assets'](character_id=character_id, page=page) for page in
+                   range(1, npages + 1)]
+            results = esi_client.multi_request(ops)
+            for request, result in results:
+                self.assets.extend(result.data)
 
         # Group Assets by Container
         containers = defaultdict(lambda: defaultdict(int))
@@ -32,8 +38,12 @@ class Assets:
                 containers[asset["location_id"]][asset["type_id"]] += asset.get("quantity", 1)
 
         # Get the name of each container
+        op = esi_app.op['post_characters_character_id_assets_names'](character_id=character_id,
+                                                                     item_ids=list(containers))
+        result = esi_client.request(op)
+
         container_names = {}
-        for item_data in asyncio.run(get_names(list(containers), character_id, access_token)):
+        for item_data in result.data:
             name = item_data["name"]
             name = name.replace("&gt;", ">")
             name = name.replace("&lt;", "<")
@@ -46,12 +56,12 @@ class Assets:
                 container_type_ids.update({asset["item_id"]: asset["type_id"]})
 
         # Get the type_name of each item and container
-        async def type_helper(type_id, name_dictionary):
-            name_dictionary.update({type_id: await get_item_name(type_id)})
+        ops = [esi_app.op['get_universe_types_type_id'](type_id=asset["type_id"]) for asset in self.assets]
+        results = esi_client.multi_request(ops)
 
         item_type_names = {}
-        tasks = [type_helper(asset["type_id"], item_type_names) for asset in self.assets]
-        asyncio.run(tqdm_asyncio.gather(*tasks))
+        for request, result in results:
+            item_type_names[result.data["type_id"]] = result.data["name"]
 
         # Finally rebuild containers with names
         self.named_containers = defaultdict(lambda: defaultdict(int))
@@ -79,21 +89,27 @@ class Assets:
         return state
 
     def check_state(self, requirement_path):
+        output = []
         """Checks the state according to the requirements and returns any mismatches"""
         with open(requirement_path, "r") as file:
             container_requirements = yaml.load(file, yaml.CLoader)
 
         for container_name, container_contents in self.named_containers.items():
-            output = ""
+            container_output = []
             if container_name in container_requirements:
                 for required_item_name, required_item_amount in container_requirements[container_name].items():
                     if not required_item_name in container_contents:
-                        output += f" - {required_item_name} is completely missing ({required_item_amount} units)\n"
+                        container_output.append(f"- {required_item_name} is completely missing ({required_item_amount} units)")
                     elif container_contents[required_item_name] < required_item_amount:
-                        output += f" - {required_item_name} is missing {required_item_amount - container_contents[required_item_name]} units\n"
+                        container_output.append(f"- {required_item_name} is missing {required_item_amount - container_contents[required_item_name]} units")
 
-            if output != "":
-                print(f"{container_name}:\n{output}")
+            if container_output:
+                output.append(
+                    f"{container_name.strip(':')}:\n" +
+                    "\n".join(container_output)
+                )
+
+        return "\n".join(output)
 
     def get_buy_list(self, requirement_path, buy_list=None, replenish_from_others=False):
 
@@ -115,11 +131,6 @@ class Assets:
             buy_list = {k: v for k, v in buy_list if v > 0}
 
         return buy_list
-
-    @staticmethod
-    def print_buy_list(buy_list):
-        for item, amount in buy_list.items():
-            print(f"{item} {amount}")
 
     def filter_state(self, ship_name):
         """Checks the state and returns the current one for a specific shipname.
---
+++ main.py
@@ -0,0 +1,191 @@
+import json
+import shelve
+import sys
+import threading
+
+import discord
+import requests
+
+from assets import Assets
+
+# Fix for Mutable Mapping collection being moved
+if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+    import collections
+
+    setattr(collections, "MutableMapping", collections.abc.MutableMapping)
+    setattr(collections, "Mapping", collections.abc.Mapping)
+
+from esipy import EsiApp
+from esipy import EsiClient
+from esipy import EsiSecurity
+
+from flask import Flask
+from flask import request
+
+# Load environment variables
+with open('secrets.json', "r") as f:
+    secrets = json.loads(f.read())
+
+# Setup Server
+flask_app = Flask(__name__)
+
+# Setup ESI
+esi_app = EsiApp().get_latest_swagger
+esi_security = EsiSecurity(
+    redirect_uri=secrets["REDIRECT_URI"],
+    client_id=secrets["CLIENT_ID"],
+    secret_key=secrets["SECRET_KEY"],
+    headers={'User-Agent': 'Hangar organizing discord bot by larynx.austrene@gmail.com'},
+)
+
+esi_client = EsiClient(
+    retry_requests=True,
+    headers={'User-Agent': 'Hangar organizing discord bot by larynx.austrene@gmail.com'},
+    security=esi_security
+)
+
+# Setup Discord
+discord_intent = discord.Intents.default()
+discord_intent.messages = True
+discord_intent.message_content = True
+discord_client = discord.Client(intents=discord_intent)
+
+
+## Server Functionality
+@flask_app.route("/")
+def hello_world():
+    return "<p>Hangar Script Callback Server</p>"
+
+
+@flask_app.route('/callback/')
+def callback():
+    # get the code from the login process
+    code = request.args.get('code')
+    author_id = request.args.get('state')
+
+    tokens = esi_security.auth(code)
+
+    character_data = esi_security.verify()
+    character_id = character_data["sub"].split(':')[-1]
+    character_name = character_data["name"]
+
+    # Store tokens under author
+    # TODO: Cross site author change fixing
+    with shelve.open('data/tokens') as author_character_tokens:
+        if author_id not in author_character_tokens:
+            author_character_tokens[author_id] = {character_id: tokens}
+        else:
+            author_character_tokens[author_id][character_id] = tokens
+
+    return f"<p>Sucessfully authentiated {character_name}!</p>"
+
+
+def get_author_characters(author_id):
+    with shelve.open('data/tokens') as author_character_tokens:
+        for character_id, tokens in author_character_tokens[str(author_id)].items():
+            esi_security.update_token(tokens)
+            tokens = esi_security.refresh()
+            author_character_tokens[str(author_id)][character_id] = tokens
+
+            character_data = esi_security.verify()
+            yield character_data["name"]
+
+
+# Discord Functionality
+def get_author_assets(author_id):
+    with shelve.open('data/tokens') as author_character_tokens:
+        for character_id, tokens in author_character_tokens[str(author_id)].items():
+            esi_security.update_token(tokens)
+            tokens = esi_security.refresh()
+            author_character_tokens[str(author_id)][character_id] = tokens
+
+            character_data = esi_security.verify()
+            character_id = character_data["sub"].split(':')[-1]
+            character_name = character_data["name"]
+
+            assets = Assets(character_id, esi_app, esi_client, character_name=character_name)
+            yield assets
+
+
+@discord_client.event
+async def on_ready():
+    print(f'We have logged in as {discord_client.user}')
+
+
+@discord_client.event
+async def on_message(message):
+    if message.author == discord_client.user:  # It is our own message
+        return
+
+    if message.content.startswith("!auth"):
+        await message.channel.send(
+            "Use the following link to authenticate characters: \n" +
+            esi_security.get_auth_uri(state=message.author.id, scopes=['esi-assets.read_assets.v1'])
+        )
+
+    if message.content.startswith("!state"):
+        try:
+            files_to_send = []
+            for assets in get_author_assets(message.author.id):
+                assets.save_state(f"data/states/{assets.character_name}.yaml")
+                with open(f"data/states/{assets.character_name}.yaml", "rb") as file:
+                    files_to_send.append(discord.File(file, filename=f"{assets.character_name}.yaml"))
+
+            await message.channel.send("Here are your current Ships", files=files_to_send)
+
+        except KeyError:
+            await message.channel.send("You have no authorized characters!")
+
+    if message.content.startswith("!check"):
+        try:
+            state_errors = []
+            for assets in get_author_assets(message.author.id):
+                state_errors.append(f"**{assets.character_name}:**\n" + assets.check_state(f"data/reqs/{message.author.id}.yaml"))
+            await message.channel.send(
+                f"**State Errors:**\n" +
+                "\n\n".join(state_errors)
+            )
+
+        except KeyError:
+            await message.channel.send("You have no authorized characters!")
+
+    if message.content.startswith("!buy"):
+        try:
+            buy_list = collections.defaultdict(int)
+            for assets in get_author_assets(message.author.id):
+                buy_list = assets.get_buy_list(f"data/reqs/{message.author.id}.yaml", buy_list=buy_list)
+            await message.channel.send(
+                f"**Buy List:**\n```" +
+                "\n".join([f"{item} {amount}" for item, amount in buy_list.items()]) +
+                "```"
+            )
+
+        except KeyError:
+            await message.channel.send("You have no authorized characters!")
+
+    if message.content.startswith("!set"):
+        try:
+            if message.attachments[0].url:
+                r = requests.get(message.attachments[0].url, allow_redirects=True)
+                with open(f"data/reqs/{message.author.id}.yaml", 'wb') as file:
+                    file.write(r.content)
+            await message.channel.send("Set new requirements file!")
+        except (KeyError, IndexError):
+            await message.channel.send("You forgot to attach a new requirement file!")
+
+    if message.content.startswith("!characters"):
+        try:
+            await message.channel.send(
+                "You have the following character(s) authenticated:\n" +
+                "\n".join(get_author_characters(message.author.id))
+            )
+        except (KeyError, IndexError):
+            await message.channel.send("You have no authorized characters!")
+
+
+if __name__ == "__main__":
+    flask_app.secret_key = secrets["FLASK_SECRET"]
+    threading.Thread(target=lambda: flask_app.run(port=5000, use_reloader=False)).start()
+    # this print a URL where we can log in
+
+    discord_client.run(secrets["TOKEN"])
